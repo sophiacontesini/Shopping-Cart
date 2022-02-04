8a3dff259e0c6f4e3a18aab7815728c5a845c409
sophiacontesini <sophiamarques214@gmail.com>
2022-02-04 18:04:34 -0300

    requisito 4 ok

--- script.js
+++ script.js
@@ -49,30 +49,45 @@ const appendCart = async () => {
   cartList.appendChild(createCartItemElement({ sku, name, salePrice }));
 };
 
+const armazenaCarrinho = [];
+
+// Requisito 4 
+// JSON.parse converte de string para objeto
+const salvaStorage = JSON.parse(getSavedCartItems());
+salvaStorage.forEach((el) => { 
+  document.querySelector('.cart__items')
+.appendChild(createCartItemElement({ sku: el.sku, name: el.name, salePrice: el.salePrice }));
+});
+//
+const addCarrim = async (event) => {
+  const id = event.target.parentNode;
+  const getItem = getSkuFromProductItem(id);
+  const getFetch = await fetchItem(getItem);
+  const { id: sku, title: name, price: salePrice } = getFetch;
+  document.querySelector('.cart__items')
+    .appendChild(createCartItemElement({ sku, name, salePrice }));
+  armazenaCarrinho.push({ sku, name, salePrice });
+  saveCartItems(JSON.stringify(armazenaCarrinho));
+  console.log(armazenaCarrinho);
+};
+
 // Requisito 2 adiciona os itens no carrinho;
 const carrim = async () => {
-const botaozim = document.querySelectorAll('.item__add');
-botaozim.forEach((el) => el.addEventListener('click', async function (event) {
-console.log(event.target); 
-const id = event.target.parentNode;
-const getItem = getSkuFromProductItem(id);
-const getFetch = await fetchItem(getItem);
-const { id: sku, title: name, price: salePrice } = getFetch;
-document.querySelector('.cart__items')
-.appendChild(createCartItemElement({ sku, name, salePrice }));
-}));
+  const botaozim = document.querySelectorAll('.item__add');
+  botaozim.forEach((el) => el.addEventListener('click', addCarrim));
 };
 
 // Requisito 1 adiciona produtos na tela;
 const appendList = async () => {
   const products = await fetchProducts('computador');
-   products.results.forEach((el) =>
+  products.results.forEach((el) =>
     document.querySelector('.items')
-    .appendChild(createProductItemElement({ sku: el.id, name: el.title, image: el.thumbnail })));
+      .appendChild(createProductItemElement({ sku: el.id, name: el.title, image: el.thumbnail })));
   await carrim();
 };
-  
-  // Sumo: usar o assync away quando for uma função, o then quando for escopo global;
-  window.onload = () => { 
-    appendList();
-  };
+
+// Sumo: usar o assync away quando for uma função, o then quando for escopo global;
+window.onload = () => {
+  appendList();
+  if (localStorage.length > 0) salvaStorage();
+};
